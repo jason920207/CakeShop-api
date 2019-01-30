77da8b1631f5ac4096196f2740b67a7b813cb06c
xiaojie zhang <xiaojiezhang@S0106441c120f48e6.ed.shawcable.net>
2019-01-30 08:14:25 -0500

    create heroku curl-scripts/sign-up

--- curl-scripts/auth/sign-in.sh
+++ curl-scripts/auth/sign-in.sh
@@ -1,3 +1,10 @@
+# @Author: xiaojiezhang
+# @Date:   2018-10-02T19:52:46-04:00
+# @Last modified by:   xiaojiezhang
+# @Last modified time: 2019-01-30T08:06:22-05:00
+
+
+
 #!/bin/bash
 
 curl "http://localhost:4741/sign-in" \
--- curl-scripts/auth/sign-up.sh
+++ curl-scripts/auth/sign-up.sh
@@ -1,6 +1,13 @@
+# @Author: xiaojiezhang
+# @Date:   2018-10-02T19:52:46-04:00
+# @Last modified by:   xiaojiezhang
+# @Last modified time: 2019-01-30T08:11:07-05:00
+
+
+
 #!/bin/bash
 
-curl "http://localhost:4741/sign-up" \
+curl "https://damp-citadel-88663.herokuapp.com/sign-up" \
   --include \
   --request POST \
   --header "Content-Type: application/json" \
