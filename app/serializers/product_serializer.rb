d7dae5e10e73729be751c3016a1571342ffeb129
xiaojie zhang <xiaojiezhang@S0106441c120f48e6.ed.shawcable.net>
2019-02-01 08:52:39 -0500

    final version

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,10 +1,11 @@
 # @Author: xiaojiezhang
 # @Date:   2019-01-29T09:17:58-05:00
 # @Last modified by:   xiaojiezhang
-# @Last modified time: 2019-01-30T11:10:06-05:00
+# @Last modified time: 2019-02-01T08:52:19-05:00
 
 
 
 class ProductSerializer < ActiveModel::Serializer
+
   attributes :id, :name, :description, :picture, :price, :allergen, :user, :category
 end
